364d71a0066e376b12f93e545f4aa507b172ddfb
Geerocktricks <geerockface4@gmail.com>
2019-02-21 13:00:35 +0300

    This commit creates the RegistrationForm class

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,15 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask import Blueprint
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 
+auth = Blueprint('auth',__name__)
 
-# auth = Blueprint('auth',__name__)
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 
@@ -18,6 +23,9 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
 
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
@@ -25,6 +33,7 @@ def create_app(config_name):
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     # Will add the views and forms
 
