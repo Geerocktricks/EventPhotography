97ddcb4896b619fcb3cd25d05b4e4380ff49556c
Geerocktricks <geerockface4@gmail.com>
2019-02-21 11:03:19 +0300

    This commit creates the roles table

--- app/models.py
+++ app/models.py
@@ -16,3 +16,13 @@ class Admin(db.Model):
 
     def __repr__(self):
         return f'User {self.username}'
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.name}'
